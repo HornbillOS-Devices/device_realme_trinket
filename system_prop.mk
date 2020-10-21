42e90d55d3382bc7ec85c2825bb85baca06eadb2
mhmmdfadly <55875784+mhmmdfadly@users.noreply.github.com>
2020-10-21 17:41:12 +0700

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -178,7 +178,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 persist.dbg.volte_avail_ovr=1 \
 persist.dbg.ims_volte_enable=1 \
 persist.dbg.vt_avail_ovr=1 \
-persist.radio.calls.on.ims=1 \ 
+persist.radio.calls.on.ims=1 \
 persist.dbg.wfc_avail_ovr=1
 
 # Vendor
