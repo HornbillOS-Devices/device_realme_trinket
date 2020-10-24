9b8df54c0fb05bc23b406943c20a703ee659ca90
mhmmdfadly <55875784+mhmmdfadly@users.noreply.github.com>
2020-10-25 01:09:20 +0700

    Update aosp_realme_trinket.mk

--- aosp_realme_trinket.mk
+++ aosp_realme_trinket.mk
@@ -14,6 +14,10 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# Face Unlock
+$(call inherit-product-if-exist, external/motorola/faceunlock/config.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := realme_trinket
@@ -23,14 +27,3 @@ PRODUCT_NAME := aosp_realme_trinket
 BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
-
-# Face Unlock
-$(call inherit-product-if-exist, external/motorola/faceunlock/config.mk)
-TARGET_FACE_UNLOCK_SUPPORTED := true
-ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
-PRODUCT_PACKAGES += \
-    FaceUnlockService
-TARGET_FACE_UNLOCK_SUPPORTED := true
-endif
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
