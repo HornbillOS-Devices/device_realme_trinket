20239c1be5f8bb6d446171aa51a9cfdf2dd37706
daralys <fadlidodo2004@gmail.com>
2020-10-31 17:08:48 +0700

    Delete RealmeParts

--- aosp_realme_trinket.mk
+++ aosp_realme_trinket.mk
@@ -11,7 +11,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, device/realme/realme_trinket/device.mk)
-$(call inherit-product, vendor/neutron/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Face Unlock
 $(call inherit-product-if-exist, external/motorola/faceunlock/config.mk)
@@ -21,10 +21,9 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := realme_trinket
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := neutron_realme_trinket
+PRODUCT_NAME := aosp_realme_trinket
 
 BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
-export NEUTRON_BUILDTYPE := OFFICIAL
